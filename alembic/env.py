a5341927681931cecf8bb0754508849da41eb342
MT-Blachetta <michaelblachetta@gmail.com>
2025-04-07 13:15:42 +0200

    renewed alembic init

--- alembic/env.py
+++ alembic/env.py
@@ -27,6 +27,7 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -58,11 +59,6 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    context.configure(
-        target_metadata=target_metadata,
-        render_as_batch=True # Add or ensure this is True
-    )
-
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -71,7 +67,7 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata, render_as_batch=True
         )
 
         with context.begin_transaction():
